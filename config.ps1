ef527641e1feb07af0d523caa2296cade2070b4d
Mahmoud shaker <95956397+0Xdarkday@users.noreply.github.com>
2024-07-12 00:30:46 +0300

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,5 +1,5 @@
 $logFile = "C:\Windows\System32\LogFiles\Firewall\pfirewall.log"
 
-
+# replace the botToken & chatID  with yours
 $botToken = "your_actual_bot_token"
 $chatID = "your_actual_chat_id"
