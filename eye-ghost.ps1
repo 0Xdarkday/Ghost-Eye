5afd2718a955a1c4caa447079f45f5872a0e1bb8
Mahmoud shaker <95956397+0Xdarkday@users.noreply.github.com>
2024-07-12 01:20:53 +0300

    Update eye-ghost.ps1

--- eye-ghost.ps1
+++ eye-ghost.ps1
@@ -1,21 +1,33 @@
-
-
+# Import necessary modules
 Import-Module -Name "$PSScriptRoot\Telegram.psm1"
 Import-Module -Name "$PSScriptRoot\LogProcessing.psm1"
 
 # Load configuration
 . "$PSScriptRoot\config.ps1"
 
-
+# Initialize an empty hash table to store IP-port mappings
 $ipPortMap = @{}
 
+# Read and process the generic log file (if exists)
+if (Test-Path $logFile) {
+    try {
+        Get-Content -Path $logFile | ForEach-Object {
+            $logEntry = $_ -split ' '
+            Process-LogEntry -logEntry $logEntry -ipPortMap $ipPortMap -botToken $botToken -chatID $chatID
+        }
+    } catch {
+        Write-Error "Failed to read or process the log file: $_"
+    }
+}
 
+# Define the event IDs to track
+$eventIDs = @(4672, 4624)
+
+# Read and process the Windows event log
 try {
-    Get-Content -Path $logFile | ForEach-Object {
-        $logEntry = $_ -split ' '
-        Process-LogEntry -logEntry $logEntry -ipPortMap $ipPortMap -botToken $botToken -chatID $chatID
-        Track-LoginEvents -logEntry $logEntry -botToken $botToken -chatID $chatID
+    Get-WinEvent -FilterHashtable @{LogName='Security'; Id=$eventIDs} | ForEach-Object {
+        Track-LoginEvents -eventRecord $_ -botToken $botToken -chatID $chatID
     }
 } catch {
-    Write-Error "Failed to read or process the log file: $_"
+    Write-Error "Failed to read or process the event log: $_"
 }
